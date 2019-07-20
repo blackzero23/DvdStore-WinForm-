2fe4037206d46a747c7814ce9ffefe8cfb294e09
Hyeong Jun Park <a46467734@gamil.com>
2019-07-20 23:02:18 +0900

    초기 커밋.

---
+++ DvdStore/Data/EntityData.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DvdStore.Data
+{
+    //where(제네릭 형식 제약 조건)(C# 참조)
+    //제네릭 정의의 where 절은 제네릭 형식, 메서드, 대리자 또는
+    //로컬 함수의 형식 매개 변수에 대한 인수로 사용되는 형식에 대한 제약 조건을 지정합니다
+    //.제약 조건은 인터페이스, 기본 클래스를 지정하거나 제네릭 형식을 참조, 값 또는 관리되지 않는
+    //형식으로 요구할 수 있습니다. 형식 인수에서 갖추고 있어야 하는 기능을 선언합니다.
+    public class EntityData<T> where T : class
+    {
+        public List<S> Select<S>(Expression<Func<T, S>> selector)
+        {
+            using (var context = new DvdStoreEntities())
+            {
+                return context.Set<T>().Select(selector).ToList();
+            }
+        }
+
+        public List<T> GetAll()
+        {
+            using (DvdStoreEntities context = new DvdStoreEntities())
+            {
+                return context.Set<T>().ToList();
+            }
+        }
+
+        public int GetCount()
+        {
+            using (DvdStoreEntities context = new DvdStoreEntities())
+            {
+                return context.Set<T>().Count();
+            }
+        }
+
+        public int GetCount(Expression<Func<T, bool>> predicate)
+        {
+            using (DvdStoreEntities context = new DvdStoreEntities())
+            {
+                return context.Set<T>().Count(predicate);
+            }
+        }
+
+        public void Insert(T entity)
+        {
+            using (DvdStoreEntities context = new DvdStoreEntities())
+            {
+                context.Set<T>().Add(entity);
+
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(T entity)
+        {
+            using (var context = new DvdStoreEntities())
+            {
+                context.Entry(entity).State = EntityState.Modified;
+
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            using (var context = new DvdStoreEntities())
+            {
+                context.Entry(entity).State = EntityState.Deleted;
+
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ DvdStore/Main.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace DvdStore
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbBook = new System.Windows.Forms.ToolStripButton();
+            this.tsbCustomer = new System.Windows.Forms.ToolStripButton();
+            this.tsbRantal = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbBook,
+            this.tsbCustomer,
+            this.tsbRantal});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbBook
+            // 
+            this.tsbBook.Image = ((System.Drawing.Image)(resources.GetObject("tsbBook.Image")));
+            this.tsbBook.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbBook.Name = "tsbBook";
+            this.tsbBook.Size = new System.Drawing.Size(54, 22);
+            this.tsbBook.Text = "Book";
+            // 
+            // tsbCustomer
+            // 
+            this.tsbCustomer.Image = ((System.Drawing.Image)(resources.GetObject("tsbCustomer.Image")));
+            this.tsbCustomer.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbCustomer.Name = "tsbCustomer";
+            this.tsbCustomer.Size = new System.Drawing.Size(79, 22);
+            this.tsbCustomer.Text = "Customer";
+            // 
+            // tsbRantal
+            // 
+            this.tsbRantal.Image = ((System.Drawing.Image)(resources.GetObject("tsbRantal.Image")));
+            this.tsbRantal.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbRantal.Name = "tsbRantal";
+            this.tsbRantal.Size = new System.Drawing.Size(60, 22);
+            this.tsbRantal.Text = "Rental";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbBook;
+        private System.Windows.Forms.ToolStripButton tsbCustomer;
+        private System.Windows.Forms.ToolStripButton tsbRantal;
+    }
+}
